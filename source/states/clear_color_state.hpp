5679b375dee3a21d733876b86710f313201a061a
Ahmed-Waleed <ahmed.ibrahim003@eng-st.cu.edu.eg>
2022-03-19 19:28:08 +0200

    clear_color_state TODO filled

--- source/states/clear_color_state.hpp
+++ source/states/clear_color_state.hpp
@@ -5,9 +5,13 @@
 class ClearColorState: public our::State {
     // onInitialize() function is called once before the state starts
     void onInitialize() override {
-        //TODO: Read the color from the configuration file and use it to set the clear color for the window
+        //DONE: Read the color from the configuration file and use it to set the clear color for the window
         //HINT: you can the configuration for the whole application using "getApp()->getConfig()"
+        const auto& config = getApp()->getConfig()["scene"]["clear-color"];
+
         //To see how the clear color is written in the json files, see "config/blue-screen.json"
+        glClearColor(config["r"], config["g"], config["b"], config["a"]);
+
         //To know how read data from a nlohmann::json object, 
         //look at the following documentation: https://json.nlohmann.me/features/element_access/
     }
